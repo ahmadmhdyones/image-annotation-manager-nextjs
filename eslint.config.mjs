705ecfcdcdbbdcb6ebad092c7495a63ee54c7896
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-01-11 22:35:39 +0400

    Setup ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable perfectionist/sort-objects */
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -12,10 +13,97 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     ignorePatterns: [],
-    plugins: [],
+    plugins: ['perfectionist'],
     extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
     rules: {
-      // TODO: Add rules
+      // General JavaScript Rules
+      'no-alert': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-destructuring': ['error', { object: true, array: false }],
+      'no-unused-vars': 'off',
+      'no-param-reassign': 'warn',
+      'no-underscore-dangle': 'off',
+      'no-restricted-exports': 'off',
+      'no-promise-executor-return': 'warn',
+
+      // React Specific Rules
+      'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
+      'react/no-children-prop': 'warn',
+      'react/no-array-index-key': 'warn',
+      'react/require-default-props': 'warn',
+      'react/jsx-props-no-spreading': 'off',
+      'react/function-component-definition': 'warn',
+
+      // TypeScript Specific Rules
+      '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+
+      // Import/Export Rules
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.test.js',
+            '**/*.test.ts',
+            '**/*.spec.js',
+            '**/*.config.ts',
+            '**/*.config.js',
+            '**/eslint.config.mjs',
+          ],
+        },
+      ],
+      'import/no-cycle': 'error',
+      'import/prefer-default-export': 'off',
+
+      // Accessibility Rules
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'jsx-a11y/control-has-associated-label': 'warn',
+
+      // Perfectionist: https://perfectionist.dev/rules
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'line-length',
+          order: 'asc',
+          groups: [
+            ['builtin', 'external'],
+            'custom-mui',
+            'custom-utils',
+            'custom-hooks',
+            'custom-components',
+            'custom-types',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown',
+          ],
+          customGroups: {
+            value: {
+              'custom-mui': '^@mui/.*',
+              'custom-hooks': '^@/hooks/.*',
+              'custom-utils': '^@/utils/.*',
+              'custom-components': '^@/components/.*',
+              'custom-types': '^@/types/.*',
+            },
+          },
+          newlinesBetween: 'always',
+          internalPattern: ['^@/.*'],
+        },
+      ],
+      'perfectionist/sort-exports': ['error', { type: 'line-length', order: 'asc' }],
+      'perfectionist/sort-named-imports': ['error', { type: 'line-length', order: 'asc' }],
+      'perfectionist/sort-named-exports': ['error', { type: 'line-length', order: 'asc' }],
+      'perfectionist/sort-enums': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-objects': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-jsx-props': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-interfaces': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-union-types': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-object-types': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-heritage-clauses': ['error', { type: 'alphabetical', order: 'asc' }],
+      'perfectionist/sort-intersection-types': ['error', { type: 'alphabetical', order: 'asc' }],
     },
   }),
 ];
