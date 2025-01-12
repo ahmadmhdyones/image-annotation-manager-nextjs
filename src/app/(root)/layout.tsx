49a4297fc557f935c5ce7a968285a7003bc92656
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-01-12 21:57:40 +0400

    Update layout component to use correct Roboto class name for styling

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${roboto.variable}`}>
+      <body className={`${roboto.className}`}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
