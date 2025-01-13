ec7e028781de7294ee49f21533e3ce3b94b80d3e
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-01-13 10:37:36 +0400

    Fix fonts.config file

--- src/configs/fonts.config.ts
+++ src/configs/fonts.config.ts
@@ -2,11 +2,13 @@ import { Roboto } from 'next/font/google';
 
 // ----------------------------------------------------------------------
 
+const roboto = Roboto({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  weight: ['300', '400', '500', '700'],
+});
+
 export const fonts = {
-  roboto: Roboto({
-    display: 'swap',
-    subsets: ['latin'],
-    variable: '--font-roboto',
-    weight: ['300', '400', '500', '700'],
-  }),
+  roboto,
 };
