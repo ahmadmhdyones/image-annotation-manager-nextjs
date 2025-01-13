e3fb46821638a880a8e9f0c53bde5f53d33d8e81
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-01-13 09:33:13 +0400

    Add navigation configuration with dashboard links and icons

---
+++ src/configs/navigation.config.tsx
@@ -0,0 +1,25 @@
+import { Navigation } from '@toolpad/core/AppProvider';
+
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import CollectionsIcon from '@mui/icons-material/Collections';
+
+import { paths } from '@/helpers/map-routes';
+
+// ----------------------------------------------------------------------
+
+export const NAVIGATION: Navigation = [
+  { kind: 'header', title: 'Dashboard' },
+  {
+    icon: <LocalOfferIcon />,
+    kind: 'page',
+    segment: paths.dashboard.categories.root.to().slice(1),
+    title: 'Categories',
+  },
+  {
+    icon: <CollectionsIcon />,
+    kind: 'page',
+    pattern: `${paths.dashboard.images.root.to().slice(1)}{/:segment}*`,
+    segment: paths.dashboard.images.root.to().slice(1),
+    title: 'Images',
+  },
+];
