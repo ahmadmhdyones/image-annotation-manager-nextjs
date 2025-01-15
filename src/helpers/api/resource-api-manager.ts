6946ac64595d082922c91611415eea8b0c800474
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-01-16 01:11:09 +0400

    Refactor ResourceApiManager to enforce type safety by using Resource's id type in getOne, update, and delete methods

--- src/helpers/api/resource-api-manager.ts
+++ src/helpers/api/resource-api-manager.ts
@@ -3,10 +3,10 @@ import { TRequest, ResourceWithoutId, TRequestGetManyOptions } from './types';
 
 // ----------------------------------------------------------------------
 
-export class ResourceApiManager<Resource> {
+export class ResourceApiManager<Resource extends { id: number | string }> {
   constructor(private readonly endpoint: string) {}
 
-  public async getOne(id: string, options?: Omit<TRequest, 'method' | 'url'>) {
+  public async getOne(id: Resource['id'], options?: Omit<TRequest, 'method' | 'url'>) {
     return await request<Resource>({ ...options, method: 'GET', url: `${this.endpoint}/${id}` });
   }
 
@@ -19,14 +19,14 @@ export class ResourceApiManager<Resource> {
   }
 
   public async update(
-    id: string,
+    id: Resource['id'],
     data: Partial<ResourceWithoutId<Resource>>,
     options?: Omit<TRequest, 'method' | 'url' | 'data'>
   ) {
     return await request<Resource>({ ...options, data, method: 'PUT', url: `${this.endpoint}/${id}` });
   }
 
-  public async delete(id: string, options?: Omit<TRequest, 'method' | 'url'>) {
+  public async delete(id: Resource['id'], options?: Omit<TRequest, 'method' | 'url'>) {
     return await request<{}>({ ...options, method: 'DELETE', url: `${this.endpoint}/${id}` });
   }
 }
