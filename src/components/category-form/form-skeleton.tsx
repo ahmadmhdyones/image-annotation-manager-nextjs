b3346815dda23358bf24f2a500718f42258ebc51
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-01-18 12:47:13 +0400

    Add FormSkeleton component for loading state representation in category form

---
+++ src/components/category-form/form-skeleton.tsx
@@ -0,0 +1,36 @@
+import { Box, Skeleton } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+export default function FormSkeleton() {
+  return (
+    <Box
+      component='section'
+      sx={{
+        bgcolor: 'background.paper',
+        borderRadius: 2,
+        boxShadow: 3,
+        margin: '0 auto',
+        maxWidth: 640,
+        minWidth: { md: 500 },
+        mt: 4,
+        p: 2,
+      }}
+    >
+      {/* Title Skeleton */}
+      <Skeleton sx={{ mb: 4, mx: 'auto', width: '200px' }} variant='text' />
+
+      {/* Name Field Skeleton */}
+      <Skeleton height={56} sx={{ mb: 2, width: '100%' }} variant='rounded' />
+
+      {/* Description Field Skeleton */}
+      <Box sx={{ mb: 2 }}>
+        <Skeleton sx={{ mb: 1, width: '100px' }} variant='text' />
+        <Skeleton height={300} sx={{ width: '100%' }} variant='rounded' />
+      </Box>
+
+      {/* Submit Button Skeleton */}
+      <Skeleton height={50} sx={{ width: '100%' }} variant='rounded' />
+    </Box>
+  );
+}
--- src/components/category-form/index.ts
+++ src/components/category-form/index.ts
@@ -1,7 +1,9 @@
 import Form from './form';
+import FormSkeleton from './form-skeleton';
 
 // ----------------------------------------------------------------------
 
 const CategoryForm = Form;
+export const CategoryFormSkeleton = FormSkeleton;
 
 export default CategoryForm;
